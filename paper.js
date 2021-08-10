2e38237268b8d4ea3360e77b8d8059c5913ccd08
somasudha <somasudha@gmail.com>
2021-08-10 12:36:29 -0700

    Update paper.js

--- paper.js
+++ paper.js
@@ -13,7 +13,7 @@ class Paper {
         
    }
    display(){
-    fill("pink");
+    fill("blue");
     ellipseMode(RADIUS);
     ellipse(this.body.position.x, this.body.position.y, this.radius, this.radius);
    }
